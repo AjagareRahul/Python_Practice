7bce858f9e3e971c76213d76397cb7c56731dfc2
Ajagare Rahul <ajagarerahul@gmail.com>
2026-03-20 11:15:18 +0530

    If else practice

---
+++ if-else.py
@@ -0,0 +1,21 @@
+n=0
+if n>0:
+    print("Positive")
+elif n<0:
+    print("Negative")
+else:    print("Zero")
+
+n=2
+if n%2==0:
+    print("Even")
+else:
+    print("Odd")
+n=10
+if n>0:
+    if n%2==0:
+        print("Positive even")
+    else:        print("Positive odd")
+elif n<0:
+    if n%2==0:
+        print("Negative even")
+    else:        print("Negative odd")  
---
+++ revise.py
@@ -0,0 +1,26 @@
+print("5"*3)
+print(5+int("3")+float("2.5"))
+
+print("5"+"3"+"2.5")
+print(True+False+True)
+print(5<10>8)
+print(not 0)
+print(not 1)
+print(-10//3)
+print(-10%3)
+a=[1,2]
+b=[1,2]
+print(a==b)
+print(a is b)
+print(id(a))
+print(id(b))
+
+print(a is not b)
+
+print(a!=b)
+print(bool("True"))
+x="10"
+y=2
+print(x*y+"5")
+n=int(input("Enter a number: "))
+print("positive" if n>0 else "negative" if n<0 else "zero" )
